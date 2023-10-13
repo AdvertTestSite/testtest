023bb46bd1817e135623438fd497fcedc3b6f0b2
AdvertTestSite <147291731+AdvertTestSite@users.noreply.github.com>
2023-10-13 09:45:00 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const clientId = 'twójClientId';
+
+// Możesz również dodać inne zmienne konfiguracyjne, jeśli to konieczne
+const redirectUri = 'https://www.twitch.tv/wysokozmineralizowanawoda';
+const scope = 'user:read:email channel:read:redemptions channel:manage:redemptions';
+
+export { clientId, redirectUri, scope };
